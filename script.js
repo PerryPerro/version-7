8ccd12afa621582d72b5e7faf097777b17c26b90
PerryPerro <andreas.persson09@gmail.com>
2017-09-21 13:55:35 +0200

    hee

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ var myTimer = setInterval(changeImage, 2000);
 $("#stopKnapp").click(function(){
     clearInterval(myTimer);
 });
-
-$("#animeKnapp").click(function(){
-    var $this = $(this);
-    if($("#animeBild").is(":visible")){
-        $("#animeBild").animate({opacity: "0", height: "0px", width: "0px"});
-        $this.text("Visa");
-    }
-    if($("#animeBild").css("opacity") == 0){
-        $("#animeBild").animate({opacity: "100", height: "100%", width: "100%"});
-        $this.text("Dölj");
-    }
-});
